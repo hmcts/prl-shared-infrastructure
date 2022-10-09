f52301b4d15c7e734dc863927c1ebfe51978eda5
Aloknath Datta <alokdatta.uit@gmail.com>
2022-10-09 03:16:16 +0100

    Updated citizen_component

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 
 module "prl-citizen-frontend-session-storage" {
   source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = "${var.product}-${var.component}-redis"
+  product  = "${var.product}-${var.citizen_component}-redis"
   location = var.location
   env      = var.env
   subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
