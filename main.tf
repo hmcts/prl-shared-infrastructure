7003adad395a4b4f3282dd640d4695e3b54b1ace
Praneeth G <praneethkumar.gadhe@hmcts.net>
2023-06-05 12:22:39 +0100

    added app_insights_connection_string

--- main.tf
+++ main.tf
@@ -125,3 +125,9 @@ resource "azurerm_key_vault_secret" "redis_access_key" {
   value        = module.prl-citizen-frontend-session-storage.access_key
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
