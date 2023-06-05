1a17a062df11e719d286e81940543ba33f270861
Praneeth G <praneethkumar.gadhe@hmcts.net>
2023-06-05 14:23:09 +0100

    added vairable for appinsights location

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-appinsights-${var.env}"
-  location            = "UK South"
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 
