60a02be96b4a938a29164ca2970cef4c98d3e798
James Turnbull <james.turnbull@justice.gov.uk>
2026-03-10 14:32:21 +0000

    Upgrade azurerm to v4.x

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
 }
 
 module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
   env     = var.env
   product = var.product
@@ -51,7 +51,7 @@ resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY_PREVIEW" {
 
 module "application_insights_preview" {
   count    = var.env == "aat" ? 1 : 0
-  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
   location = var.location
   env      = "preview"
   product  = var.product
