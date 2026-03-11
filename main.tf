6a0e4435f26ee8465948af6840642ac69f12decc
James Turnbull <james.turnbull@justice.gov.uk>
2026-03-11 14:36:28 +0000

    Move app insights to separate file

--- main.tf
+++ main.tf
@@ -19,54 +19,6 @@ module "key-vault" {
   create_managed_identity = true
 }
 
-resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
-  name         = "AppInsightsInstrumentationKey"
-  value        = module.application_insights.instrumentation_key
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-module "application_insights" {
-  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
-
-  env     = var.env
-  product = var.product
-  name    = "${var.product}-appinsights"
-
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  common_tags         = var.common_tags
-}
-
-moved {
-  from = azurerm_application_insights.appinsights
-  to   = module.application_insights.azurerm_application_insights.this
-}
-
-resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY_PREVIEW" {
-  name         = "AppInsightsInstrumentationKey-Preview"
-  value        = module.application_insights_preview[0].instrumentation_key
-  key_vault_id = module.key-vault.key_vault_id
-  count        = var.env == "aat" ? 1 : 0
-}
-
-module "application_insights_preview" {
-  count    = var.env == "aat" ? 1 : 0
-  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
-  location = var.location
-  env      = "preview"
-  product  = var.product
-  name     = "${var.product}-appinsights"
-
-  resource_group_name = azurerm_resource_group.rg.name
-
-  common_tags = var.common_tags
-}
-
-moved {
-  from = azurerm_application_insights.appinsights_preview[0]
-  to   = module.application_insights_preview[0].azurerm_application_insights.this
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}" # update these values if required
   resource_group_name = "${var.product}-${var.env}" # update these values if required
@@ -162,9 +114,3 @@ resource "azurerm_key_vault_secret" "redis_access_key" {
 #  value        = module.prl-citizen-frontend-secondary-session-storage.access_key
 #  key_vault_id = data.azurerm_key_vault.key_vault.id
 #}
-
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = module.application_insights.connection_string
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
